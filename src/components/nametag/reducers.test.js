6503bd9f9e0cbc0abb54b5524bd79b83ba226278
injoongyoon <27440940+AttilaTheHen@users.noreply.github.com>
2018-09-07 13:29:30 -0700

    Add test for adding a greeting and verify it fails

--- src/components/nametag/reducers.test.js
+++ src/components/nametag/reducers.test.js
@@ -1,9 +1,20 @@
-import { greeting, name } from './reducers';
+import { greeting, name, GREETING_ADD } from './reducers';
 
 describe('Greeting reducers', () => {
   it('initializes to hello', () => {
     const state = greeting(undefined, {});
     expect(state).toBe('hello');
+  });
+
+  it('adds a greeting', () => {
+    const newGreeting = 'howdy';
+    
+    const state = greeting('', {
+      type: GREETING_ADD,
+      payload: newGreeting
+    });
+
+    expect(state).toBe('howdy');
   });
 });
 
