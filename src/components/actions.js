59c980d2323ee4e56e9018804bbb2dfff41d32a5
Kevin Grayson <k.grayson.ms@gmail.com>
2018-09-07 13:54:17 -0700

    No test files

--- src/components/actions.js
+++ src/components/actions.js
@@ -1,9 +1,11 @@
 import { GREETING_CHANGE, NAME_CHANGE } from './reducers';
 
-export const changeGreeting = () => ({
+export const changeGreeting = greeting => ({
   type: GREETING_CHANGE,
+  payload: greeting
 });
 
-export const changeName = () => ({
+export const changeName = name => ({
   type: NAME_CHANGE,
+  payload: name
 });
