c75afddb08bc19f107b311b02908b6689142f713
Mark Myers <markalope@gmail.com>
2018-09-07 13:23:45 -0700

    created reducer

--- src/components/reducers.js
+++ src/components/reducers.js
@@ -1,1 +1,13 @@
 export const NAMETAG_CREATE = 'NAMETAG_CREATE';
+
+export function nametag(state = [], { type, payload }) {
+  switch(type) {
+    case NAMETAG_CREATE:
+      return [
+        ...state,
+        payload
+      ];
+    default:
+      return state;
+  }
+}
