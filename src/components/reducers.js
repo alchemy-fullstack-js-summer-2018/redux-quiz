34d73cbec947ad1e9b9cbfb192be06efe71b5e7d
Arthur Jen <arthurljen@gmail.com>
2018-09-07 13:16:56 -0700

    added store and reducers

---
+++ src/components/reducers.js
@@ -0,0 +1,13 @@
+export const GREETING_CHANGE = 'GREETING_CHANGE';
+export const NAME_CHANGE = 'NAME_CHANGE';
+
+export const getGreeting = state => state.greeting;
+export const getName = state => state.Name;
+
+export function greeting(state = 'Hello', { type, payload }) {
+  if(type === GREETING_CHANGE) return payload;
+}
+
+export function name(state = '', { type, payload }) {
+  if(type === NAME_CHANGE) return payload;
+}
