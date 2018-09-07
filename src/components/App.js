98f68f36e524e78f1ca4c6be4f722e1935837022
MariahAdams <mariah.adams23@yahoo.com>
2018-09-07 13:21:05 -0700

    wiring up controls component

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
+import Controls from './Controls';
 
 class App extends Component {
 
   render() {
 
     return (
-      <div>Hello Redux Quiz!</div>
+      <div>
+        Hello Redux Quiz!
+        <Controls/>  
+      </div>
     );
   }
 }
---
+++ src/components/Controls.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+// import PropTypes from 'prop-types';
+
+class Controls extends Component {
+
+  render() {
+
+    return (
+      <section>
+        controls Component
+      </section>
+    );
+  }
+}
+
+export default Controls;
