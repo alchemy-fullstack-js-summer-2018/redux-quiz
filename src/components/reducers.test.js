885b15a3d66e7a37ef2f060ab95951ba86eb8aac
Robyn Navarro <rnavarro182@gmail.com>
2018-09-07 13:42:27 -0700

    name change test passing

--- src/components/reducers.test.js
+++ src/components/reducers.test.js
@@ -6,8 +6,29 @@ import {
 } from './reducers';
 
 describe('reducers', () => {
-  it('inits to Hello', () => {
+  it('inits greeting to Hello', () => {
     const state = greeting(undefined, {});
     expect(state).toEqual('Hello');
+  });
+
+  it('inits name to empty string', () => {
+    const state = name(undefined, {});
+    expect(state).toEqual('');
+  });
+
+  it('it changes greeting', () => {
+    const state = greeting('', {
+      type: GREETING_CHANGE,
+      payload: 'Greetings'
+    });
+    expect(state).toEqual('Greetings');
+  })
+
+  it('changes name', () => {
+    const state = name('Robyn', {
+      type: NAME_CHANGE,
+      payload: 'Hood'
+    });
+    expect(state).toEqual('Hood');
   });
 });
