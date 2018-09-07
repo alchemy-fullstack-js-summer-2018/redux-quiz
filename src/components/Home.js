76ab1492f2a32020cbe9830014c132218f5dd052
Mark Myers <markalope@gmail.com>
2018-09-07 13:20:20 -0700

    starting on actions file

---
+++ src/components/Home.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Home extends Component {
+  render() { 
+    return (
+      <div>
+        <h2>Nametags!</h2>
+        <p>Edit your Nametag</p>
+      </div>
+    );
+  }
+}
+ 
+export default Home;
---
+++ src/components/reducers.js
@@ -0,0 +1,1 @@
+export const NAMETAG_CREATE = 'NAMETAG_CREATE';
