f9ced21191e7fbbfc66a83e7dcfea4b520814bc9
injoongyoon <27440940+AttilaTheHen@users.noreply.github.com>
2018-09-07 13:57:32 -0700

    Make Display component

---
+++ src/components/nametag/Display.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+
+class Display extends Component {
+  static propTypes = {
+    greeting: PropTypes.string,
+    name: PropTypes.string
+  };
+
+  render() { 
+    const { greeting, name } = this.props;
+    return (
+      <section>
+        <p>{greeting}</p>
+        <p>{name}</p>
+      </section>
+    );
+  }
+}
+ 
+export default connect(
+  null,
+  null
+)(Display);
