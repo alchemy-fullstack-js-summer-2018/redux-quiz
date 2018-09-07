09d8c5c9af7ef2b49001a5dcb8c928bbab4a9fce
Arthur Jen <arthurljen@gmail.com>
2018-09-07 13:41:47 -0700

    added selectors to Display component

--- src/components/Display.js
+++ src/components/Display.js
@@ -1,16 +1,30 @@
 import React, { PureComponent } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
+import { getName, getGreeting } from './reducers';
 
 class Display extends PureComponent {
-  state = {  }
+
+  static propTypes = {
+    greeting: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired
+  };
+
   render() { 
+    const { greeting, name } = this.props;
+
     return (
       <section>
-        Hello Stranger
+        {greeting} {name}
       </section>
     );
   }
 }
  
-export default Display;
+export default connect(
+  state => ({
+    name: getName(state),
+    greeting: getGreeting(state)
+  }),
+  null
+)(Display);
