e0711f7f308d62943323e918c827b6bc908d1165
MariahAdams <mariah.adams23@yahoo.com>
2018-09-07 13:49:41 -0700

    first actions test passing

---
+++ src/components/actions.js
@@ -0,0 +1,8 @@
+import { GREETING_CHANGE/* , NAME_CHANGE */ } from './reducers';
+
+export const changeGreeting = greeting => {
+  return {
+    type: GREETING_CHANGE,
+    payload: greeting
+  };
+};
--- src/components/actions.test.js
+++ src/components/actions.test.js
@@ -1,8 +1,12 @@
-
+import { GREETING_CHANGE } from './reducers';
+import { changeGreeting } from './actions';
 
 describe('action creators', () => {
 
-  it('', () => {
-    
+  it('creates an actions that changes the greeting', () => {
+    const greeting = 'Hello';
+    const { type, payload } = changeGreeting(greeting);
+    expect(type).toBe(GREETING_CHANGE);
+    expect(payload).toBe(greeting);  
   });
 });
