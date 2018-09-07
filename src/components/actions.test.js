8c7f6cb6098830897347665aadc2cabb5bba9bfb
Antreo Pukay <acrosswalker@yahoo.com>
2018-09-07 13:58:24 -0700

    added sisplay and scaffold for tests

---
+++ src/components/actions.test.js
@@ -0,0 +1,4 @@
+import { load, add, updateGreeting, updateName } from './actions';
+import { DISPLAY_LOAD, DISPLAY_ADD, DISPLAY_UPDATE } from './reducers';
+
+describe('')
---
+++ src/components/reducers.test.js
@@ -0,0 +1,8 @@
+import { DISPLAY_LOAD, DISPLAY_ADD, DISPLAY_UPDATE } from './reducers';
+
+describe('Display reducers', () => {
+
+  it('greeting field defaults to hello', () => {
+
+  });
+});
