b5bd6af4f608078634c13640131de20d622931f5
MariahAdams <mariah.adams23@yahoo.com>
2018-09-07 13:34:10 -0700

    second reducer test passing

--- src/components/reducers.js
+++ src/components/reducers.js
@@ -1,7 +1,10 @@
 
+export const GREETING_CHANGE = 'GREETING_CHANGE';
 
-export function greeting(state = 'Hello', { type }) {
+export function greeting(state = 'Hello', { type, payload }) {
   switch(type) {
+    case GREETING_CHANGE:
+      return payload;
     default: 
       return state;
   }
--- src/components/reducers.test.js
+++ src/components/reducers.test.js
@@ -1,9 +1,18 @@
-import { greeting } from './reducers';
+import { greeting, GREETING_CHANGE } from './reducers';
 
 describe('reducers', () => {
 
   it('defaults to hello', () => {
     const state = greeting('Hello', {});
     expect(state).toBe('Hello');
+  });
+
+  it('returns new greeting', () => {
+    const data = { greeting: 'Guten Morgen' };
+    const state = greeting('', {
+      type: GREETING_CHANGE,
+      payload: data
+    });
+    expect(state).toBe(data);
   });
 });
