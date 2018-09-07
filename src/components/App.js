eced9a4b9e6719fa19d64954f5dad20a97f6031e
Antreo Pukay <acrosswalker@yahoo.com>
2018-09-07 14:00:53 -0700

    final commit allowed

--- src/components/App.js
+++ src/components/App.js
@@ -6,6 +6,13 @@ class App extends Component {
   render() {
 
     return (
+      <Router>
+        <Switch>
+              <Route exact path="/" component={Home}/>
+              <Route exact path="/display" component={Display}/>
+              <Redirect to="/"/>
+            </Switch>
+      </Router>
       <div>Hello Redux Quiz!</div>
       
     );
