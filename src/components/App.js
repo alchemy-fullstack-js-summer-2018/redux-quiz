f54cbcec87fb217d19ec35c40f2c2fabad3fd7c9
martypdx <marty@martypdx.com>
2018-09-22 15:30:49 -0700

    App.js setup

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,7 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import logo from '../assets/logo.png';
+import RainbowLightBulb from './RainbowLightBulb';
+import ColorChooser from './ColorChooser';
 import './App.css';
 
 class App extends Component {
@@ -7,10 +9,16 @@ class App extends Component {
   render() {
 
     return (
-      <header>
-        <img src={logo}/>
-        <h1>React &amp; Redux Quiz</h1>
-      </header>
+      <Fragment>
+        <header>
+          <img src={logo}/>
+          <h1>React &amp; Redux Quiz</h1>
+        </header>
+        <main>
+          <RainbowLightBulb/>
+          <ColorChooser/>
+        </main>
+      </Fragment>
     );
   }
 }
