d1f1f9ab30babba39ca817d02bd4f4400dc10dd6
martypdx <marty@martypdx.com>
2018-09-22 15:23:55 -0700

    Alchemy Template Style

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
+import logo from '../assets/logo.png';
+import './App.css';
 
 class App extends Component {
 
   render() {
 
     return (
-      <div>Hello Redux Quiz!</div>
+      <header>
+        <img src={logo}/>
+        <h1>React &amp; Redux Quiz</h1>
+      </header>
     );
   }
 }
